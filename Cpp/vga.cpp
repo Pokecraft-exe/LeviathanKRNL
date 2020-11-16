8bace342c575af356cf1e611f69f95eef61fcac2
Pokecraft-exe <67156699+Pokecraft-exe@users.noreply.github.com>
2020-11-16 20:44:32 +0100

    Add files via upload

---
+++ Cpp/vga.cpp
@@ -0,0 +1,113 @@
+#include "H/vga.h"
+
+void WriteRegisters(uint8* registers)
+{
+    //  misc
+    outb(0x3C2, *(registers++));
+
+    // sequencer
+    for(uint8 i = 0; i < 5; i++)
+    {
+        outb(0x3C4, i);
+        outb(0x3C5, *(registers++));
+    }
+
+    // cathode ray tube controller
+    outb(0x3d4, 0x03);
+    outb(0x3d5, inb(0x3d5) | 0x80);
+    outb(0x3d4, 0x11);
+    outb(0x3d5,  inb(0x3d5) & ~0x80);
+
+    registers[0x03] = registers[0x03] | 0x80;
+    registers[0x11] = registers[0x11] & ~0x80;
+
+    for(uint8 i = 0; i < 25; i++)
+    {
+        outb(0x3d4, i);
+        outb(0x3d5, *(registers++));
+    }
+
+    // graphics controller
+    for(uint8 i = 0; i < 9; i++)
+    {
+        outb(0x3ce, i);
+        outb(0x3cf, *(registers++));
+    }
+
+    // attribute controller
+    for(uint8 i = 0; i < 21; i++)
+    {
+        inb(0x3da);
+        outb(0x3c0, i);
+        outb(0x3c0, *(registers++));
+    }
+
+    inb(0x3da);
+    outb(0x3c0, 0x20);
+
+}
+
+bool SupportsMode(uint32 width, uint32 height, uint32 colordepth)
+{
+    return width == 320 && height == 200 && colordepth == 8;
+}
+
+bool SetMode(uint32 width, uint32 height, uint32 colordepth)
+{
+    if(!SupportsMode(width, height, colordepth))
+        return false;
+
+    unsigned char g_320x200x256[] =
+    {
+        /* MISC */
+            0x63,
+        /* SEQ */
+            0x03, 0x01, 0x0F, 0x00, 0x0E,
+        /* CRTC */
+            0x5F, 0x4F, 0x50, 0x82, 0x54, 0x80, 0xBF, 0x1F,
+            0x00, 0x41, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0x9C, 0x0E, 0x8F, 0x28, 0x40, 0x96, 0xB9, 0xA3,
+            0xFF,
+        /* GC */
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40, 0x05, 0x0F,
+            0xFF,
+        /* AC */
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+            0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+            0x41, 0x00, 0x0F, 0x00, 0x00
+    };
+	
+    WriteRegisters(g_320x200x256);
+    return true;
+
+uint8* GetFrameBufferSegment()
+{
+    graphicsControllerIndexPort.Write(0x06);
+    uint8 segmentNumber = graphicsControllerDataPort.Read() & (3<<2);
+    switch(segmentNumber)
+    {
+        default:
+        case 0<<2: return (uint8_t*)0x00000;
+        case 1<<2: return (uint8_t*)0xA0000;
+        case 2<<2: return (uint8_t*)0xB0000;
+        case 3<<2: return (uint8_t*)0xB8000;
+    }
+}
+
+void PutPixel(uint32 x, uint32 y,  uint8 colorIndex)
+{
+    uint8* pixelAddress = GetFrameBufferSegment() + 320*y + x;
+    *pixelAddress = colorIndex;
+}
+
+uint8 GetColorIndex(uint8 r, uint8 g, uint8 b)
+{
+    if(r == 0x00, g == 0x00, b == 0xA8)
+        return 0x01;
+    return 0x00;
+}
+
+void PutPixel(uint32 x, uint32 y,  uint8 r, uint8 g, uint8 b)
+{
+    PutPixel(x,y, GetColorIndex(r,g,b));
+}
