bdd2c17935513ab49ff8be3da0766dd91a5822ca
Julian LF <67156699+Pokecraft-exe@users.noreply.github.com>
2020-12-15 15:53:22 +0100

    Update IDT.cpp

--- Cpp/IDT.cpp
+++ Cpp/IDT.cpp
@@ -2,33 +2,37 @@
 
 extern IDT64 _idt[256];
 extern uint_64 isr1;
-extern uint_64 isr12;
+extern uint_64 irq12;
 extern "C" void LoadIDT();
+
+int IRQ_offset = 80;
 
 void(*MainKeyboardHandler)(uint_8 scanCode, uint_8 chr);
 
 void InitIDT(){
-    //for (int i = 0; i <= 256, i++ ;){
-	    _idt[1].zero = 0;
-	    _idt[1].offset_low = (uint_16)(((uint_64)&isr1 & 0x000000000000ffff));
-	    _idt[1].offset_mid = (uint_16)(((uint_64)&isr1 & 0x00000000ffff0000) >> 16);
-	    _idt[1].offset_high = (uint_32)(((uint_64)&isr1 & 0xffffffff00000000) >> 32);
-	    _idt[1].ist = 0;
-	    _idt[1].selector = 0x08;
-	    _idt[1].types_attr = 0x8e;
-	    _idt[12].zero = 0;
-	    _idt[12].offset_low = (uint_16)(((uint_64)&isr12 & 0x000000000000ffff));
-	    _idt[12].offset_mid = (uint_16)(((uint_64)&isr12 & 0x00000000ffff0000) >> 16);
-	    _idt[12].offset_high = (uint_32)(((uint_64)&isr12 & 0xffffffff00000000) >> 32);
-	    _idt[12].ist = 0;
-	    _idt[12].selector = 0x08;
-	    _idt[12].types_attr = 0x8e;
-
-	    RemapPic();
-    //}
-    outb(PIC1_DATA, 0b11111101);
-    outb(PIC2_DATA, 0b11101111);
+	InitISR(1, &isr1, 0, 0x08, 0x8e);
+	InitIRS(12, &irq12, 0, 0x08, 0x8e);
+	RemapPic(IRQ_offset, IRQ_offset+8);
+	outb(PIC1_DATA, 0b11111101);
+	outb(PIC2_DATA, 0b11101111);
 	LoadIDT();
+}
+
+void InitISR(uint8 num, void * handler, uint8 ist, uint16 selector, uint8 attributes){
+  	_idt[num].zero = 0;
+	_idt[num].offset_low = (uint16)(((uint64) handler & 0x000000000000ffff));
+	_idt[num].offset_mid = (uint16)(((uint64) handler & 0x00000000ffff0000) >> 16);
+	_idt[num].offset_high = (uint32)(((uint64) handler & 0xffffffff00000000) >> 32);
+	_idt[num].ist = ist;
+	_idt[num].selector = selector;
+	_idt[num].types_attr = attributes;
+	return;
+}
+
+void installIRQ(uint8 IRQ, void *handler, uint8 ist, uint16 selector, uint8 attributes)
+{
+	InitISR(IRQ+IRQ_offset, handler, ist, selector, attributes);
+	return;
 }
 
 extern "C" void isr1_handler(){
@@ -44,4 +48,8 @@ extern "C" void isr1_handler(){
 
 	outb(0x20, 0x20);
 	outb(0xa0, 0x20);*/
+}
+
+extern "C" void irq12_handler(){
+	//mouse
 }
