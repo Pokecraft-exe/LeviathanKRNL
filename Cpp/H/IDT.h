ad8297e8867a0d14aa629923b0c07378bcf918bb
Pokecraft-exe <osdev@serrv.localdomain>
2021-04-07 11:06:28 +0200

    automatik push

--- Cpp/H/IDT.h
+++ Cpp/H/IDT.h
@@ -47,17 +47,19 @@ class IDT
 private:
   IDTR idtr;
 public:
-  void SetIDTGate(void* Handler, uint8_t offset, uint8_t type_attr, uint8_t selector){
-    IDT64* idt = (IDT64*)(idtr.Offset + offset * sizeof(IDT64));
-    idt->Set_Offset((uint64_t)Handler);
-    idt->types_attr = type_attr;
-    idt->selector = selector;
-  }
   void InitIDT(){
     idtr.Limit = 0x0FFF;
     idtr.Offset = (uint64_t)malloc(4096);
 
-    SetIDTGate((void*)isr1_handler, 0x21, IDT_IG, 0x08);
+    IDT64* PF = (IDT64*)(idtr.Offset + 0xE * sizeof(IDT64));
+    PF->Set_Offset((uint64_t)pagefault);
+    PF->types_attr = IDT_IG;
+    PF->selector = 0x08;
+
+    IDT64* Keyboard = (IDT64*)(idtr.Offset + 0x21 * sizeof(IDT64));
+    Keyboard->Set_Offset((uint64_t)isr1_handler);
+    Keyboard->types_attr = IDT_IG;
+    Keyboard->selector = 0x08;
 
     asm ("lidt %0" :: "m"(idtr));
 
--- Cpp/IDT.cpp
+++ Cpp/IDT.cpp
@@ -16,3 +16,26 @@ __attribute__((interrupt)) void isr1_handler(struct interrupt_frame* frame){
 	outb(0x20, 0x20);
 	outb(0xa0, 0x20);
 }
+
+__attribute__((interrupt)) void pagefault(struct interrupt_frame* frame){
+	"\033[1;31m"
+	write_serial('\033');
+	write_serial('[');
+	write_serial('1');
+	write_serial(';');
+	write_serial('3');
+	write_serial('1');
+	write_serial('m');
+	write_serial('p');
+	write_serial('a');
+	write_serial('g');
+	write_serial('e');
+	write_serial(' ');
+	write_serial('f');
+	write_serial('a');
+	write_serial('u');
+	write_serial('l');
+	write_serial('t');
+	write_serial('!');
+	while(true);
+}
