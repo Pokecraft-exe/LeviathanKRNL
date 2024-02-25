d8814ccbd4cff95ac74a268db83c20d29f39b714
Julian Lavis--Fabbri <leviathancorp.officiel@gmail.com>
2024-02-25 10:42:06 -0500

    Delete src directory

--- src/lalgorythm.cpp
+++
@@ -1,9 +0,0 @@
-namespace std {
-    namespace __private_std {
-        void memcpy(void* dest, void* src, int len) {
-            for (int i = 0; i < len; i++) {
-                *((char*)dest + i) = *((char*)src + i);
-            }
-        }
-    }
-}
